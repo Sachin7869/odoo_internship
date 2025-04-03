416d21e3ea086c809c2b4e97a18f41a87d51ef9c
sachin14 <sachinmadakari05@gmail.com>
2025-04-03 13:30:37 +0530

    samplem model

---
+++ sample_module/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'sample_module',
+    'version': '18.0.0.0',
+    'summary': 'madre mia',
+    'description': """ This Module for training purposes.
+    """,
+    'category':'',
+    'author': 'Sachin',
+    'website': 'www.zbeanztech.com',
+    "license": "LGPL-3",
+    'depends': [],
+    'data': [
+				'views/model_one_view.xml',
+				'views/menu.xml',
+				
+        ],
+    'test': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
