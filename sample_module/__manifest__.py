9f7456f3436718e5863e62d393dff2d42b4aa555
sachin14 <sachinmadakari05@gmail.com>
2025-04-16 22:23:32 +0530

    added pivot,graph,calender,list,kanban

--- sample_module/__manifest__.py
+++ sample_module/__manifest__.py
@@ -1,17 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Odoo, Open Source Management Solution
+# Copyright (C) 2025 ZestyBeanz Technologies(<http://www.zbeanztech.com>)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 {
     'name': 'sample_module',
     'version': '18.0.0.0',
     'summary': 'madre mia',
-    'description': """ This Module for training purposes.
+    'description': """ This Module  is for training purposes.
     """,
-    'category':'',
-    'author': 'Sachin',
+    'category':'Training',
+    'author': 'Sachin T',
     'website': 'www.zbeanztech.com',
     "license": "LGPL-3",
-    'depends': [],
+    'depends': ['base','sale','sale_management','account','contacts','product'],
     'data': [
-				'views/model_one_view.xml',
-				'views/menu.xml',
+				'security/ir.model.access.csv',
+                'security/security.xml',
+                'data/sequence.xml',
+                'views/car_rental_view.xml',
+                'views/food_view.xml',
+                'views/model_one_view.xml',
+                'views/model_one_lines.xml',
+                'views/menu.xml',
 				
         ],
     'test': [],
